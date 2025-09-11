20770d53884955e0da969aa6f299ad46d7d8ab30
brandonprunty001 <brandon.prunty001@albright.edu>
2025-09-11 11:14:31 -0400

    Add files via upload

---
+++ assignments/01_getting_started/01_01_python_org.py
@@ -0,0 +1,6 @@
+# starting python 1
+""""
+i learned that pyhton is free open source for people
+python community alows anyone to use it 
+python is a great community and i will recomend anyone to use it
+"""
---
+++ assignments/01_getting_started/01_03_infinite_skills.py
@@ -0,0 +1,3 @@
+# this is the beging on what i want to achive 
+# i want to do some password cracking 
+#
