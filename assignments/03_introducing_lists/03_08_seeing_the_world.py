72c6e6796e3cf489d0ac668822cef9da83a1a1e2
brandonprunty001 <brandon.prunty001@albright.edu>
2025-09-18 12:58:05 -0400

    Add files via upload

---
+++ assignments/03_introducing_lists/03_08_seeing_the_world.py
@@ -0,0 +1,24 @@
+cities = ["Egpyt", "Tokyo", "Rome", "London", "paris"]
+
+print("Original list of cities:")
+print (cities)
+
+print("InFirst city in the list:", cities[0])
+print ("Last city in the list:", cities [-1])
+
+cities.append ("Rome")
+print("InList after adding a city with append():")
+print(cities)
+
+cities. insert(2, "Sydney")
+print ("InList after inserting a city at position 2 with insert():")
+print(cities)
+
+removed_city = cities.pop()
+print(f"\nRemoved city with pop(): (removed_city)")
+print ("List after pop():") 
+print(cities)
+
+cities, remove("Tokyo")
+print ("InList alter removing 'Tokyo' with remove():")
+print(cities)
---
+++ assignments/03_introducing_lists/03_09_dinner_guests.py
@@ -0,0 +1,2 @@
+guests = ["Big baby", "Dom", "Ben", "Maine", "Angel","manman" ]
+print (f"i am inviting {len(guests)}people to dinner")
