17446675fb6b9f5444ac2259ec01cea3c7b88f1d
brandonprunty001 <brandon.prunty001@albright.edu>
2025-09-18 12:57:16 -0400

    Delete assignments/03_introducing_lists/03_07_shrinking_guest_list.py

--- assignments/03_introducing_lists/03_07_shrinking_guest_list.py
+++
@@ -1,5 +0,0 @@
-guests = ["kendall", "nick", "cam", "fioma", "angel"]
-
-print("unfortunately, the new dinner table wont ariive in time for the dinner, so i can only invite a couple of people")
-
-while len(guests) > 2:
